11b4b53105632d3a2fed8fad699c998279f85bea
FabioCinicolo <f.cinicolo@studenti.unina.it>
2020-12-27 16:49:17 +0100

    Update Query.sql

--- root/Documentazione/BD/Query.sql
+++ root/Documentazione/BD/Query.sql
@@ -1,214 +1,271 @@
 -- TABELLA SCORTA
-CREATE TABLE Scorta(
 
-	cod_negozio CHAR(3),
-	cod_alimento CHAR(6),
-	quantita SMALLINT NOT NULL,
+CREATE TABLE Supply(
 
-	PRIMARY KEY(cod_negozio, cod_alimento),
-	FOREIGN KEY (cod_negozio) REFERENCES Negozio(cod_negozio) ON DELETE CASCADE,
-	FOREIGN KEY (cod_alimento) REFERENCES Alimento(cod_alimento) ON DELETE CASCADE
-	
+	shop_id CHAR(4),
+	food_id CHAR(6),
+	quantity SMALLINT NOT NULL,
+
+	PRIMARY KEY(shop_id, food_id),
+	FOREIGN KEY (shop_id) REFERENCES Shop(shop_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (food_id) REFERENCES Food(food_id) ON DELETE CASCADE ON UPDATE CASCADE
 )
 
 
 -- TABELLA COMPORDINE
-CREATE TABLE CompOrdine(
 
-	cod_ordine CHAR(16),
-	cod_alimento CHAR(6),
-	quantita SMALLINT NOT NULL,
+CREATE TABLE OrderComposition(
 
-	PRIMARY KEY(cod_ordine, cod_alimento),
-	FOREIGN KEY (cod_ordine) REFERENCES Ordine(cod_ordine) ON DELETE CASCADE,
-	FOREIGN KEY (cod_alimento) REFERENCES Alimento(cod_alimento)
-	
+	order_id CHAR(12),
+	food_id CHAR(6),
+	quantity SMALLINT NOT NULL,
+
+	PRIMARY KEY(order_id, food_id),
+	FOREIGN KEY (order_id) REFERENCES CustomerOrder(order_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (food_id) REFERENCES Food(food_id) ON UPDATE CASCADE
 )
 
 
+
 -- TABELLA COMPCARRELLO
-CREATE TABLE CompCarrello(
+CREATE TABLE CartComposition(
 
-	cod_carrello CHAR(16),
-	cod_alimento CHAR(6),
-	quantita SMALLINT NOT NULL,
-
-	PRIMARY KEY(cod_carrello, cod_alimento),
-	FOREIGN KEY (cod_carrello) REFERENCES Carrello(cod_carrello) ON DELETE CASCADE,
-	FOREIGN KEY (cod_alimento) REFERENCES Alimento(cod_alimento) ON DELETE CASCADE
+	cart_id CHAR(12),
+	food_id CHAR(6),
+	quantity SMALLINT NOT NULL,
 	
+	PRIMARY KEY(cart_id, food_id),
+	FOREIGN KEY (cart_id) REFERENCES Cart(cart_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (food_id) REFERENCES Food(food_id) ON DELETE CASCADE ON UPDATE CASCADE
 )
 
 
 -- TABELLA CONTRATTO
-CREATE TABLE Contratto(
 
-	cod_rider CHAR(5),
-	cod_negozio CHAR(3),
+CREATE TABLE Contract(
+
+	rider_id CHAR(5),
+	shop_id CHAR(4),
 	
-	PRIMARY KEY (cod_rider,cod_negozio),
-	FOREIGN KEY (cod_rider) REFERENCES Rider(cod_rider) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (cod_negozio) REFERENCES Negozio(cod_negozio) ON DELETE CASCADE ON UPDATE CASCADE
+	PRIMARY KEY (rider_id,shop_id),
+	FOREIGN KEY (rider_id) REFERENCES Rider(rider_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (shop_id) REFERENCES Shop(shop_id) ON DELETE CASCADE ON UPDATE CASCADE
 	
 )
 
 
 -- TABELLA COMPALIMENTO
-CREATE TABLE CompAlimento( 
 
-	cod_alimento CHAR(8), 
-	t_allergene TipoAllergene, 
+CREATE TABLE FoodComposition( 
 
-	PRIMARY KEY(cod_alimento,t_allergene), 
-	FOREIGN KEY (cod_alimento) REFERENCES Alimento(cod_alimento) ON DELETE CASCADE, 
-	FOREIGN KEY (t_allergene) REFERENCES Allergene(t_allergene) 
+	food_id CHAR(6), 
+	allergen AllergenType, 
+
+	PRIMARY KEY(food_id,allergen), 
+	FOREIGN KEY (food_id) REFERENCES Food(food_id) ON DELETE CASCADE ON UPDATE CASCADE, 
+	FOREIGN KEY (allergen) REFERENCES Allergen(allergen) ON DELETE CASCADE ON UPDATE CASCADE
 
 ) 
 
 -- TABELLA ALLERGENE
-CREATE TYPE TipoAllergene AS ENUM ('Cereali e derivati', 'Crostacei', 'Uova', 'Pesce', 'Arachidi', 'Soia', 'Latte', 'Frutta a guscio', 'Sedano', 'Senape', 'Anidride solforosa e solfiti', 'Lupini', 'Molluschi');
-CREATE TABLE Allergene( 
+CREATE TYPE AllergenType AS ENUM ('Cereali e derivati', 'Crostacei', 'Uova', 'Pesce', 'Arachidi', 'Soia', 'Latte', 'Frutta a guscio', 'Sedano', 'Senape', 'Anidride solforosa e solfiti', 'Lupini', 'Molluschi');
+CREATE TABLE Allergen( 
 
-	t_allergene TipoAllergene PRIMARY KEY 
+	allergen AllergenType PRIMARY KEY 
 
 ) 
 
 
 -- TABELLA ALIMENTO
-CREATE TABLE Alimento( 
+CREATE TABLE Food( 
 
-	cod_alimento CHAR (6) PRIMARY KEY, 
-	nome VARCHAR (32) NOT NULL UNIQUE, 
-	prezzo REAL NOT NULL, 
-	descrizione VARCHAR(255) 
-
+    food_id CHAR (6) PRIMARY KEY, 
+	name VARCHAR (32) NOT NULL UNIQUE, 
+	price REAL NOT NULL, 
+	description VARCHAR(255) 
 ) 
 
 
 -- TABELLA ORDINE
-CREATE TYPE StatoConsegna AS ENUM ('In attesa','In consegna','Consegnato','Errore');
-CREATE TABLE Ordine(
 
-	cod_ordine CHAR(16) PRIMARY KEY,
-	data_ordine TIMESTAMP NOT NULL,
-	ora_consegna TIME,
-	indirizzo VARCHAR(255) NOT NULL,
-	stato StatoConsegna NOT NULL DEFAULT 'In attesa',
+CREATE TYPE PaymentType AS ENUM ('Contrassegno','Carta');
+CREATE TYPE DeliveryStatus AS ENUM ('In attesa','In consegna','Consegnato','Errore');
+
+CREATE TABLE CustomerOrder(
+
+	order_id CHAR(12) PRIMARY KEY,
+	order_date TIMESTAMP NOT NULL,
+	delivery_time TIME,
+	address VARCHAR(255) NOT NULL,
+	status DeliveryStatus NOT NULL DEFAULT 'In attesa',
+	payment PaymentType,
 	note VARCHAR(255),
-	cod_rider CHAR(5),
-	cod_negozio CHAR(3),
-	cod_utente CHAR(10),
+	rider_id CHAR(5),
+	shop_id CHAR(4),
+	customer_id CHAR(10),
 	
-	FOREIGN KEY(cod_rider) REFERENCES Rider(cod_rider),
-	FOREIGN KEY(cod_negozio) REFERENCES Negozio(cod_negozio),
-	FOREIGN KEY(cod_utente) REFERENCES Utente(cod_utente)
-	
-)
+	FOREIGN KEY(rider_id) REFERENCES Rider(rider_id),
+	FOREIGN KEY(shop_id) REFERENCES Shop(shop_id),
+	FOREIGN KEY(customer_id ) REFERENCES Customer(customer_id)
+	)
+
+
 
 
 -- TABELLA CARRELLO
-CREATE SEQUENCE seq_Carrello; 
+CREATE SEQUENCE Cart_sequence; 
 
-CREATE TABLE Carrello( 
+CREATE TABLE Cart( 
 
-	cod_carrello CHAR(16) PRIMARY KEY DEFAULT to_char(nextval('seq_carrello'),'0000000000000000'), 
-	completo CHAR(1) NOT NULL, 
-	cod_utente CHAR(10), 
-
-	CONSTRAINT fk_Carrello FOREIGN KEY(cod_utente) REFERENCES Utente(cod_utente) ON DELETE CASCADE ON UPDATE CASCADE 
-
-)
-
-
--- TABELLA PERSONA
-CREATE TABLE Persona( 
-
-	cf CHAR(16), 
-	nome VARCHAR(30) NOT NULL, 
-	cognome VARCHAR(30) NOT NULL, 
-	residenza VARCHAR(255) NOT NULL, 
-	data_n DATE NOT NULL, 
-	luogo_n VARCHAR(25) NOT NULL, 
-	sesso CHAR(1) NOT NULL, 
-	cellulare CHAR(10) NOT NULL, 
-
-	CONSTRAINT pk_Persona PRIMARY KEY (cf) 
+	cart_id CHAR(12) PRIMARY KEY DEFAULT to_char(nextval('Cart_sequence'),'000000000000'), 
+	complete CHAR(1) NOT NULL DEFAULT 'n', 
+	customer_id CHAR(10), 
+	FOREIGN KEY(customer_id) REFERENCES Customer(customer_id) ON DELETE CASCADE ON UPDATE CASCADE 
 
 )
+
+
+
 
 
 -- TABELLA RIDER
-CREATE TYPE TipoVeicolo AS ENUM ('Bicicletta', 'Motoveicolo', 'Autovettura');
-CREATE SEQUENCE seq_Rider; 
+
+CREATE TYPE VehicleType AS ENUM ('Bicicletta', 'Motoveicolo', 'Autovettura');
+CREATE SEQUENCE Rider_sequence; 
 CREATE TABLE Rider( 
 
-	cod_rider CHAR(5) PRIMARY KEY DEFAULT to_char(nextval ('seq_rider'),'R0000FM'), 
+	cf CHAR(16) NOT NULL UNIQUE, 
+	name VARCHAR(32) NOT NULL, 
+	surname VARCHAR(32) NOT NULL, 
+	address VARCHAR(255) NOT NULL, 
+	birth_date DATE NOT NULL, 
+	birth_place VARCHAR(25) NOT NULL, 
+	gender CHAR(1) NOT NULL, 
+	cellphone CHAR(10) NOT NULL UNIQUE, 
+	rider_id CHAR(5) PRIMARY KEY DEFAULT to_char(nextval ('Rider_sequence'),'R0000FM'), 
 	email VARCHAR(320) NOT NULL UNIQUE, 
 	password VARCHAR(32) NOT NULL, 
-	veicolo TipoVeicolo NOT NULL, 
-	fascia_oraria CHAR(11) NOT NULL, 
-	n_consegne SMALLINT NOT NULL, 
-	cf CHAR(16), 
-
-	CONSTRAINT fk_Rider FOREIGN KEY (cf) REFERENCES Persona(cf) ON DELETE CASCADE ON UPDATE CASCADE 
-
+	vehicle VehicleType NOT NULL, 
+	working_time CHAR(11) NOT NULL, 
+	deliveries_number SMALLINT NOT NULL
+	
 ) 
 
 
+
 -- TABELLA UTENTE
-CREATE SEQUENCE seq_Utente; 
+CREATE SEQUENCE Customer_sequence;
 
-CREATE TABLE Utente( 
-
-	cod_utente CHAR(10) PRIMARY KEY DEFAULT to_char(nextval('seq_Utente'),'U000000000FM'), 
+CREATE TABLE Customer( 
+    
+	name VARCHAR(32) NOT NULL, 
+	surname VARCHAR(32) NOT NULL, 
+	address VARCHAR(255) NOT NULL, 
+	birth_date DATE NOT NULL, 
+	birth_place VARCHAR(25) NOT NULL, 
+	gender CHAR(1) NOT NULL, 
+	cellphone CHAR(10) NOT NULL UNIQUE, 
+	customer_id CHAR(10) PRIMARY KEY DEFAULT to_char(nextval('Customer_sequence'),'U000000000FM'), 
 	email VARCHAR(320) NOT NULL UNIQUE, 
 	password VARCHAR(32) NOT NULL, 
-	flag CHAR(1) NOT NULL, 
-	cf CHAR(16), 
-
-	CONSTRAINT fk_Utente FOREIGN KEY(cf) REFERENCES Persona(cf) ON DELETE CASCADE ON UPDATE CASCADE 
-
+	flag CHAR(1) NOT NULL DEFAULT 'b', 
+	cf CHAR(16) NOT NULL UNIQUE
+	
 )
 
 
+
 -- TABELLA NEGOZIO
-CREATE SEQUENCE seq_Negozio;
+CREATE SEQUENCE Shop_sequence;
 
- CREATE TABLE Negozio(
-
-	cod_negozio CHAR(3) PRIMARY KEY DEFAULT to_char(nextval('seq_Negozio'),'000'),
-	nome VARCHAR(32) NOT NULL,
-	indirizzo VARCHAR(255) NOT NULL,
-	orario CHAR(11),
-	giorno_chiusura VARCHAR(56)
-
+ CREATE TABLE Shop(
+	 
+	shop_id CHAR(4) PRIMARY KEY DEFAULT to_char(nextval('Shop_sequence'),'S000'),
+	name VARCHAR(32) NOT NULL,
+	address VARCHAR(255) NOT NULL UNIQUE,
+	working_hours CHAR(11),
+	closing_days VARCHAR(56)
  )
-
 
 
 --TRIGGER E FUNCTION PER CREARE UN ORDINE
 
 
-CREATE TRIGGER crea_ordine
-AFTER UPDATE ON Carrello
+CREATE TRIGGER createOrder
+AFTER UPDATE ON Cart
 FOR EACH ROW
-WHEN (OLD.completo='n' AND NEW.completo='s')
-EXECUTE PROCEDURE crea_ordine();
+WHEN (OLD.complete='n' AND NEW.complete='s')
+EXECUTE PROCEDURE createOrder();
 
-
-CREATE OR REPLACE FUNCTION crea_ordine() RETURNS trigger AS $crea_ordine$
+//Creazione ordine
+CREATE OR REPLACE FUNCTION createOrder() RETURNS trigger AS $createOrder$
 DECLARE
 /* Inizializzare indirizzo, note, cod_rider, cod_negozio*/
 BEGIN
-INSERT INTO Ordine (cod_ordine,cod_utente, data_ordine,indirizzo ,note , cod_rider, cod_negozio)
-VALUES(NEW.cod_carrello,NEW.cod_utente,current_timestamp,indirizzo ,note ,cod_rider, cod_negozio);
-INSERT INTO CompOrdine (SELECT * FROM CompCarrello WHERE cod_carrello=NEW.cod_carrello);
-UPDATE Scorta AS S
-SET quantita = S.quantita - C.quantita
-FROM CompOrdine AS C
-WHERE S.cod_alimento = C.cod_alimento AND C.cod_ordine=NEW.cod_carrello AND S.cod_negozio = cod_negozio;
-DELETE FROM Carrello WHERE cod_carrello=NEW.cod_carrello;
+INSERT INTO Order (order_id,user_id, order_date,address ,note , rider_id, shop_id, status)
+VALUES(NEW.cart_id,NEW.user_id,DEFAULT,address ,note ,rider_id, shop_id, DEFAULT);
+INSERT INTO OrderComposition (SELECT * FROM CartComposition WHERE cart_id=NEW.cart_id);
+UPDATE Supply AS S
+SET quantity = S.quantity - OC.quantity
+FROM OrderComposition AS OC
+WHERE S.food_id = OC.food_id AND OC.order_id=NEW.cart_id AND S.shop_id = shop_id;
+DELETE FROM Cart WHERE cart_id=NEW.cart_id;
 RETURN NEW;
 END;
 $crea_ordine$ LANGUAGE plpgsql;
 
+//Inserisce un alimento nella tabella Food
+CREATE OR REPLACE PROCEDURE insertFood(name_food varchar, prefix varchar, price real, description varchar) LANGUAGE PLPGSQL AS $$
+declare
+suffix CHAR(4)=to_char(nextval('Food_sequence'),'0000FM');
+begin
+INSERT INTO Food VALUES (prefix||suffix, name_food, price, description);
+end;
+$$;
+ 
+//Inserisce un allergene ad un alimento
+CREATE OR REPLACE PROCEDURE insertAllergen(food_id varchar, allergen AllergenType ) LANGUAGE PLPGSQL AS $$
+begin
+INSERT INTO FoodComposition VALUES(food_id,allergen);
+end;
+$$;
+ 
+//Inserisce un rider
+CREATE OR REPLACE PROCEDURE insertRider(cf varchar, rider_name varchar, surname varchar, address varchar, birth_date date, birth_place varchar, gender char, cellphone  varchar,
+										email varchar, password varchar, vehicle VehicleType, working_time varchar, deliveries_number Smallint, shop_id varchar) 
+LANGUAGE PLPGSQL AS $$
+BEGIN
+INSERT INTO Rider VALUES (cf, rider_name, surname, address, birth_date, birth_place, gender, cellphone, DEFAULT, email, password, vehicle, working_time, deliveries_number);
+INSERT INTO Contract VALUES (to_char(currval('Rider_sequence'),'R0000FM'), shop_id);
+END;
+$$;
+
+//Eliminazione di un rider
+CREATE OR REPLACE PROCEDURE deleteRider(cod_rider varchar) 
+LANGUAGE PLPGSQL AS $$
+BEGIN
+DELETE FROM Rider WHERE rider_id = cod_rider;
+END;
+$$;
+
+//Inserisce un negozio
+CREATE OR REPLACE PROCEDURE insertShop(shop_name varchar, address varchar, working_hours varchar, closing_days varchar) LANGUAGE PLPGSQL AS $$
+BEGIN
+INSERT INTO Shop VALUES(DEFAULT, shop_name, address, working_hours, closing_days);
+END;
+$$;
+
+
+//Eliminazione di un negozio
+CREATE OR REPLACE PROCEDURE deleteShop(cod_shop varchar) LANGUAGE PLPGSQL AS $$
+BEGIN
+DELETE FROM Shop WHERE shop_id = cod_shop;
+END;
+$$;
+
+//Eliminazione di un alimento
+CREATE OR REPLACE PROCEDURE deleteFood(food_name varchar) LANGUAGE PLPGSQL AS $$
+BEGIN
+DELETE FROM Food WHERE name = food_name;
+END;
+$$;
