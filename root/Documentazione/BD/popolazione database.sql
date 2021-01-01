ea9a12be29bed4d68b4e81daec15e5bc1068045d
Vonrisen <68206419+Vonrisen@users.noreply.github.com>
2021-01-01 14:47:30 +0100

    Sistemazione popolazione.sql

---
+++ root/Documentazione/BD/popolazione database.sql
@@ -0,0 +1,140 @@
+	--INSERIMENTO IN CUSTOMER
+INSERT INTO customer VALUES (DEFAULT, 'Pasquale', 'De Nicola', 'Via Di Pasquale', '1970-09-18', 'Napoli', 'M', '1234567890', 'p.denicola@outlook.com', 'Password123', 'DNCPQL70P18F839P');
+
+INSERT INTO customer VALUES(DEFAULT,'Marica','Savino','Via DI Marica','1975-02-06','Caserta','F','2345678901','m.savino@outlook.com','MaricaSa975','SVNMRC75B46B963V');
+
+INSERT INTO customer VALUES(DEFAULT,'Nicola','Trinchese','Via Avella','1996-01-14','Avella','M','3456789012','n.trinchese@libero.it','trinCo1','TRNNCL96A14A508U');
+
+INSERT INTO customer VALUES(DEFAULT,'Annalisa','Fiocchi','Via Sicura','1986-04-22','Sorrento','F','4567890123','a.fiocchi@alice.it','LisaFiocchiAnna32','FCCNLS86D62I862G');
+
+INSERT INTO customer VALUES(DEFAULT,'Mario','Parisi','Via Bono','2000-11-17','Avellino','M','5678901234','m.parisi@live.it','PaMario00','PRSMRA00S17A509N');
+
+INSERT INTO customer VALUES(DEFAULT,'Elena','De Luca','Via Daqui','1999-12-31','Salerno','F','6789012345','e.deluca@outlook.it','DEelena99','DLCLNE99T71H703K');
+
+INSERT INTO customer VALUES(DEFAULT,'Saverio','Romano','Via Oleandri','2001-05-19','Pozzuoli','M','7890123456','s.romano@libero.com','SaRoVeMa201','RMNSVR01E19G964E');
+
+INSERT INTO customer VALUES(DEFAULT,'Giovanna','Di Maio','Via Seconda','1980-08-08','Nola','F','8901234567','d.dimaio@outlook.com','gioDima80','DMIGNN80M48F924S');
+
+INSERT INTO customer VALUES(DEFAULT,'Carmine','Ioime','Via Trivice','1990-03-29','Pomigliano D'arco','M','9012345678','c.ioime@outlook.com','Mino90','MIOCMN90C29G812U');
+
+INSERT INTO customer VALUES(DEFAULT,'Cristina','D'avena','Via Zecchino d'Oro','1964-07-06','Bologna','F','0123456789','c.davena@puffi.pu','Puffiamo10','DVNCST64L46A944B');
+
+
+
+	--INSERIMENTO IN MEAL
+INSERT INTO meal VALUES (DEFAULT,'Spaghetti e vongole',5.50,'Ingredienti:Spaghetti, vongole, pomodorini');
+
+INSERT INTO meal VALUES (DEFAULT,'Spaghetti alla Carbonara',10.00,'Ingredienti:Spaghetti, tuorli, guanciale, pepe nero, pecorino romano');
+
+INSERT INTO meal VALUES (DEFAULT,'Pennette all'arrabiata',8.50,'Ingredienti:Penne rigate, aglio, prezzemolo, sale, pomodori pelati, peperoncino, olio');
+
+INSERT INTO meal VALUES (DEFAULT,'Brodo di tortellini',11.00,'Ingredienti:Tortellini, brodino con spezie varie');
+
+INSERT INTO meal VALUES (DEFAULT,'Cotoletta',8.50,'Ingredienti:Carne di vitello, uova, farina, burro');
+
+INSERT INTO meal VALUES (DEFAULT,'Carne alla pizzaiola',11.50,'Ingredienti:Fettine di manzo, olio, sale, aglio, passata di pomodoro, origano, pepe');
+
+INSERT INTO meal VALUES (DEFAULT,'Costine al forno',12.00,'Ingredienti:Costine di carne, rosmarino, pepe nero, aglio');
+
+INSERT INTO meal VALUES (DEFAULT,'Baccalà fritto',10.50,'Ingredienti:Baccalà dissalato, farina');
+
+INSERT INTO meal VALUES (DEFAULT,'Insalata di polpo e patate',9.00,'Ingredienti:Polpo, Patate, prezzemolo, olio, succo di limone');
+
+INSERT INTO meal VALUES (DEFAULT,'Salmone croccante',9.50,'Ingredienti:Filetto di salmone, prezzemolo, scorza di limone, pane, pepe bianco, rosmarino');
+
+INSERT INTO meal VALUES (DEFAULT,'CocaCola',2.00,'Ingredienti:Acqua, Anidride carbonica, zucchero, colorante, acido fosforico, aromi naturali. caffeina');
+
+INSERT INTO meal VALUES (DEFAULT,'Bottiglina d'acqua',1.00);
+
+INSERT INTO meal VALUES (DEFAULT,'Patatine Fritte p',2.00,'Porzione piccola. Ingredienti:Patate, sale, olio');
+
+INSERT INTO meal VALUES (DEFAULT,'Patatine Fritte m',3.00,'Porzione Merdia. Ingredienti:Patate, sale, olio');
+
+INSERT INTO meal VALUES (DEFAULT,'Patatine Fritte g',4.00,'Porzione Grande. Ingredienti:Patate, sale, olio');
+
+INSERT INTO meal VALUES (DEFAULT,'Panino hamburger',6.00,'Ingredienti:Panino, hamburger, patatine, pomodorini');
+
+INSERT INTO meal VALUES (DEFAULT,'Patatine kebab',7.00,'Ingredienti:Panino, patatine, provola');
+
+INSERT INTO meal VALUES (DEFAULT,'Pizza margherita',4.00);
+
+INSERT INTO meal VALUES (DEFAULT,'Gelato alla vaniglia',2.00);
+
+INSERT INTO meal VALUES (DEFAULT,'Gelato al cioccolato',2.00);
+
+INSERT INTO meal VALUES (DEFAULT,'Gelato a fragola',2.00);
+
+INSERT INTO meal VALUES (DEFAULT,'Monoporzione red velvet',5.00,'Ingredienti:Berro, uova, farina, zucchero, sale, coloranti, yogurt bianco, lievito');
+
+INSERT INTO meal VALUES (DEFAULT,'Pasticceria mignon',6.00,'Ingredienti:vari');
+
+
+
+
+	--INSERIMENTO IN RIDER
+INSERT INTO rider VALUES (DEFAULT,'FRUGLC90T02F839Q','Gianluca','Fru','Via Napoli','1990-12-02','Napoli','M','0987654321','Bicicletta','00:00-00:00');
+
+INSERT INTO rider VALUES (DEFAULT,'RZZLNZ00B63C675H','Lorenza','Rozza','Via Marino','2000-02-23','Cicciano','F','9876543210','Motoveicolo','00:00-00:00');
+
+INSERT INTO rider VALUES (DEFAULT,'DLRFNC93C03H703Q','Francesco','Di Lauro','Via Ester','1993-03-03','Salerno','M','8765432109','Autoveicolo','00:00-00:00');
+
+INSERT INTO rider VALUES (DEFAULT,'LTRMRA96A58F839V','Maria','Altieri','Via Cite','1996-01-18','Napoli','F','7654321098','Bicicletta','00:00-00:00');
+
+INSERT INTO rider VALUES (DEFAULT,'CRDMHL99A10B963H','Michele','Cordaro','Via De Stefano','1999-01-10','Caserta','M','6543210987','Motoveicolo','00:00-00:00');
+
+INSERT INTO rider VALUES (DEFAULT,'MLLMRC91S52C188Y','Mariarca','Maiello','Via Cisterna','1991-11-12','Castello di Cisterna','F','5432109876','Autoveicolo','00:00-00:00');
+
+INSERT INTO rider VALUES (DEFAULT,'TRCLGU88E11A509X','Luigi','Turco','Via Nonsaprei','1988-05-11','Avellino','M','4321098765','Motoveicolo','00:00-00:00');
+
+INSERT INTO rider VALUES (DEFAULT,'MNDVLR96C63F924G','Valeria','Menditti','Via Cimitile','1996-03-23','Nola','F','3210987654','Motoveicolo','00:00-00:00');
+
+INSERT INTO rider VALUES (DEFAULT,'TSTNGL97L15G964R','Angelo','Testa','Via Campo','1997-07-15','Pozzuoli','M','2109876543','Autoveicolo','00:00-00:00');
+
+INSERT INTO rider VALUES (DEFAULT,'NPLCMN01P70G964X','Carmen','Napolitano','Via Stens','2001-09-30','Pozzuoli','F','1098765432','Motoveicolo','00:00-00:00');
+
+
+
+
+
+	--INSERIMENTO IN SHOP
+INSERT INTO shop VALUES (DEFAULT, 'Food Overflow Napoli', 'Via Napoli');
+
+INSERT INTO shop VALUES (DEFAULT, 'Food Overflow Caserta', 'Via Caserta');
+
+INSERT INTO shop VALUES (DEFAULT, 'Food Overflow Salerno', 'Via Salerno');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
