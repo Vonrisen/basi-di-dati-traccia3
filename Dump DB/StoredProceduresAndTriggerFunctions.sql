d7b77c0629d0fe35be117b98b9f635af443903d0
FabioCinicolo <f.cinicolo@studenti.unina.it>
2021-02-03 12:39:34 +0100

    Update File delle query
    1) Sistemate stored procedures in funzione degli aggiornamenti effettuati sui metadati delle tabelle;
    2) Aggiunti constraint relativi a:
     -Email degli shop\rider\cf;
     -Working_hours degli shop\rider
     -Telefono di casa dello shop
     -Codice fiscale degli utenti\rider

---
+++ Dump DB/StoredProceduresAndTriggerFunctions.sql
@@ -0,0 +1,134 @@
+  --Creazione ordine
+--meal_list_name e' la lista dei nomi dei cibi ordinati dall' utente  / quantity_list e' la lista delle quantita' dei rispettivi cibi
+CREATE OR REPLACE PROCEDURE createOrder(address varchar, payment varchar, note varchar, shop_id varchar, customer_id varchar, meal_list_name varchar, quantity_list varchar) 
+LANGUAGE PLPGSQL AS $$
+DECLARE
+meal_name Meal.name%TYPE;
+quant varchar;
+meal_counter int default 1;
+id_meal Meal.id%TYPE;
+BEGIN
+IF note='' THEN note=null; END IF;
+INSERT INTO CustomerOrder VALUES (DEFAULT, DEFAULT, null, address, DEFAULT, payment, note, null, shop_id, customer_id);
+LOOP
+ meal_name=split_part(meal_list_name, ', ', meal_counter);
+ quant=split_part(quantity_list, ', ', meal_counter);
+ SELECT id INTO id_meal FROM Meal WHERE name=meal_name;
+ IF meal_name<>'' THEN
+ INSERT INTO OrderComposition VALUES(to_char(currval('customerorder_sequence'),'000000000000FM'),id_meal,quant::real);
+ meal_counter=meal_counter+1;
+ ELSE
+ exit;
+ END IF;
+END LOOP;
+END;
+$$
+
+
+--Aggiornamento rider
+CREATE OR REPLACE PROCEDURE updateRider(cod_f varchar, residence varchar, cellular varchar, vehic varchar, workingtime varchar)
+AS $$
+BEGIN
+IF workingtime=''THEN workingtime=null; END IF;
+UPDATE Rider SET address=residence, cellphone=cellular, vehicle=vehic, working_time=workingtime WHERE cf=cod_f;
+END;
+$$
+LANGUAGE PLPGSQL;
+
+
+
+--Update Shop
+CREATE OR REPLACE PROCEDURE updateShop(shop_name varchar, address varchar, working_hours varchar, closing_days varchar, password varchar, oldEmail varchar, newEmail varchar, phone varchar)
+AS $$
+BEGIN
+IF working_hours=''THEN working_hours=DEFAULT; END IF;
+IF closing_days=''THEN closing_days=null; END IF;
+UPDATE Shop SET shop_name=shop_name, working_hours=working_hours, closing_days=closing_days, password=password, email=newEmail, home_phone=phone
+WHERE email=oldEmail;
+END;
+$$
+LANGUAGE PLPGSQL;
+
+
+
+
+--Dato un alimento e una lista di allergeni, aggiunge questi ultimi all' alimento
+CREATE OR REPLACE PROCEDURE addAllergens(meal_name varchar, allergens varchar) LANGUAGE plpgsql AS $$
+DECLARE
+allerg Allergen.name%TYPE DEFAULT '';
+count_allergens INT DEFAULT 1;
+meal_id Meal.id%TYPE;
+BEGIN
+SELECT id INTO meal_id FROM meal WHERE name=meal_name;
+LOOP
+ allerg=split_part(allergens, ', ', count_allergens);
+ IF allerg<>'' THEN
+ INSERT INTO MealComposition VALUES(meal_id, allerg);
+ count_allergens = count_allergens + 1;
+ ELSE
+ exit;
+ END IF;
+END LOOP;
+END;
+$$;
+
+
+CREATE OR REPLACE FUNCTION getAllergensofameal(meal varchar) RETURNS VARCHAR  language plpgsql AS $$
+DECLARE
+my_curs cursor FOR SELECT allergen_name FROM MealComposition WHERE meal_id=meal;
+allergens Allergen.name%TYPE DEFAULT '';
+BEGIN
+FOR I IN my_curs
+LOOP
+allergens = i.allergen_name||', '||allergens;
+END LOOP;
+return substr(allergens,1,length(allergens)-2);
+END; 
+$$
+
+--
+CREATE OR REPLACE FUNCTION associateRiderToOrder() RETURNS TRIGGER AS $associate_rider_to_order$
+BEGIN
+	
+		if (SELECT deliveries_number FROM rider WHERE cf=new.rider_cf) < 3
+		THEN
+			UPDATE Rider
+			SET deliveries_number=deliveries_number+1
+			WHERE cf=new.rider_cf;
+			
+			new.status='In consegna';
+		END IF;
+	
+	return new;
+
+END;
+$associate_rider_to_order$ LANGUAGE plpgsql;
+
+CREATE TRIGGER associate_rider_to_order
+BEFORE UPDATE of rider_cf ON customerorder
+FOR EACH ROW
+WHEN (NEW.rider_cf IS NOT NULL)
+EXECUTE PROCEDURE add_rider_in_order();
+
+
+
+CREATE OR REPLACE FUNCTION updateDeliveriesNumberOfARider() RETURNS TRIGGER AS $update_deliveries_number_of_a_rider$
+BEGIN
+	
+	IF new.status = 'Consegnato' OR new.status = 'Errore'
+	THEN
+	
+		UPDATE rider
+		SET deliveries_number=deliveries_number-1
+		WHERE cf = new.rider_cf;
+		
+	END IF;
+	
+	RETURN NEW;
+	
+END;
+$update_deliveries_number_of_a_rider$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_deliveries_number_of_a_rider
+AFTER UPDATE of status ON customerorder
+FOR EACH ROW EXECUTE PROCEDURE update_deliveries_number_of_a_order();
--- Dump DB/TablesAndConstraints.sql
+++ Dump DB/TablesAndConstraints.sql
@@ -7,7 +7,8 @@ CREATE SEQUENCE letter START WITH 65 MINVALUE 65 MAXVALUE 90;
 
 CREATE TABLE customer (
 
-	cf CHAR(16) PRIMARY KEY,
+        id CHAR(8) PRIMARY KEY DEFAULT to_char(nextval('customer_sequence'),'00000000FM'),
+	cf CHAR(16),
 	name VARCHAR(50) NOT NULL,
 	surname VARCHAR(50) NOT NULL,
 	address VARCHAR(255) NOT NULL,
@@ -15,7 +16,6 @@ CREATE TABLE customer (
 	birth_place VARCHAR(64) NOT NULL,
 	gender CHAR(1) NOT NULL,
 	cellphone CHAR(10) NOT NULL UNIQUE,
-	
 	email VARCHAR(320) NOT NULL UNIQUE,
 	password VARCHAR(32) NOT NULL
 	
@@ -29,10 +29,10 @@ CREATE TABLE customer (
 	name VARCHAR(50) NOT NULL,
 	address VARCHAR(255) NOT NULL UNIQUE,
 	working_hours CHAR(11) DEFAULT '00:00-24:00',
-	closing_days VARCHAR(62),
+	closing_days VARCHAR(62) DEFAULT '',
 	email VARCHAR(320) NOT NULL UNIQUE,
-	password VARCHAR(32) NOT NULL
-
+	password VARCHAR(32) NOT NULL,
+        cellphone CHAR(10) NOT NULL UNIQUE
 );
 
 -- Creazione tabella "Rider"
@@ -83,11 +83,11 @@ CREATE TABLE customerorder (
 	
 	rider_cf CHAR(16),
 	shop_id CHAR(3),
-	customer_cf CHAR(16),
+	customer_id CHAR(8),
 	
 	FOREIGN KEY(rider_cf) REFERENCES Rider(cf),
 	FOREIGN KEY(shop_id) REFERENCES Shop(id),
-	FOREIGN KEY(customer_cf) REFERENCES Customer(cf)
+	FOREIGN KEY(customer_id) REFERENCES Customer(id)
 	
 );
 
@@ -163,10 +163,47 @@ ADD CONSTRAINT shop_address_check CHECK (address~*'^(accesso|arco|belvedere|borg
 												 spiaggia|spiazzo|strada|stradale|stretto|stretta|strettoia|terrazza|traversa|via|vicoletto|vico|villaggio|viuzza)( )[ ''A-Za-z0-9]+(, )[ A-Z0-9]+(, )[0-9]{5}(, )[A-Za-z]+(, )[A-Z]{2}$' );
 
 
---Constraint Email
+--Constraint email customer
 ALTER TABLE Customer
-ADD CONSTRAINT customer_email_check CHECK (email::text ~* '^[A-Za-z0-9._%-]+(@)[A-Za-z0-9.-]+[.][A-Za-z]+$'::text)
+ADD CONSTRAINT customer_email_check CHECK (email ~* '^[A-Za-z0-9._%-]+(@)[A-Za-z0-9.-]+[.][A-Za-z]+$'::text);
 
---Constraint closing days shop
+ALTER TABLE Customer
+ADD CONSTRAINT customer_cf_check CHECK (cf ~* '^[a-zA-Z]{6}[0-9]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9]{2}([a-zA-Z]{1}[0-9]{3})[a-zA-Z]{1}$')
+
+--Constraint closing_days shop
 ALTER TABLE Shop 
-ADD CONSTRAINT shop_closing_days_check CHECK (closing_days ~* '^((lunedì|martedì|mercoledì|giovedì|venerdì|sabato|domenica)(, (?!$)|$))*$')
+ADD CONSTRAINT shop_closing_days_check CHECK (closing_days ~* '^((lunedì|martedì|mercoledì|giovedì|venerdì|sabato|domenica)(, (?!$)|$))*$');
+
+--Constraint working_hours shop
+ALTER TABLE Shop
+ADD CONSTRAINT shop_working_hours_check CHECK (working_hours ~* '^([0-2]{1}[0-9]{1})(:)([0-5]{1}[0-9]{1})(-)([0-2]{1}[0-9]{1})(:)([0-5]{1}[0-9]{1})$');
+
+--Constraint email shop
+ALTER TABLE Shop
+ADD CONSTRAINT shop_email_check CHECK (email ~* '^[A-Za-z0-9._%-]+(@foodoverflow.it)$');
+
+--Constraint working_hours rider
+ALTER TABLE Rider 
+ADD CONSTRAINT rider_working_hours_check CHECK (working_hours ~* '^([0-2]{1}[0-9]{1})(:)([0-5]{1}[0-9]{1})(-)([0-2]{1}[0-9]{1})(:)([0-5]{1}[0-9]{1})$');
+
+--Constraint codice fiscale rider
+ALTER TABLE Rider
+ADD CONSTRAINT rider_cf_check CHECK (cf ~* '^[a-zA-Z]{6}[0-9]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9]{2}([a-zA-Z]{1}[0-9]{3})[a-zA-Z]{1}$')
+
+--Constraint cellphone customer
+ALTER TABLE Customer
+ADD CONSTRAINT customer_cellphone_check CHECK (cellphone ~ '^[0-9]{10}$');
+
+--Constraint cellphone rider
+ALTER TABLE Rider
+ADD CONSTRAINT rider_cellphone_check CHECK (cellphone ~ '^[0-9]{10}$');
+
+--Constraint home_phone
+ALTER TABLE Shop
+ADD CONSTRAINT shop_home_phone_check CHECK (home_phone ~ '^(004191|010|011|0121|0122|0123|0124|0125|0131|0141|0142|0143|0144|015|0161|0163|0165|0166|0171|0172|0173|0174|0175|0182|0183|0184|0185|0187|019|02|030|031|0321|0322|0323|0324|0331|0332|0341|0342|0343|
+0344|0345|0346|035|0362|0363|0364|0365|0371|0372|0373|0374|0375|0376|0377|0381|0382|0383|0384|0385|0386|039|040|041|0421|0422|0423|0424|0425|0426|0427|0428|0429|0431|0432|0433|0434|0435|0436|0437|0438|
+0439|0442|0444|0445|045|0461|0462|0463|0464|0465|0471|0472|0473|0474|0481|049|050|051|0521|0522|0523|0524|0525|0532|0533|0534|0535|0536|0541|0542|0543|0544|0545|0546|0547|0549|055 0564|0565|0566|0571|
+0572|0573|0574|0575|0577|0578|0583|0584|0585|0586|0587|0588|059 06|0623|070 071|0721|0722|0731|0732|0733|0734|0735|0736|0737|0742|0743|0744|0746|075|0761|0763|0765|0766|0771|0773|0774|0775|0776|0781|
+0782|0783|0784|0785|0789|079|080|081|0823|0824|0825|0827|0828|0831|0832|0833|0835|0836|085|0861|0862|0863|0864|0865|0871|0872|0873|0874|0875|0881|0882|0883|0884|0885|089|090|091|0921|0922|0923|
+0924|0925|0931|0932|0933|0934|0935|0941|0942|095|0961|0962|0963|0964|0965|0966|0967|0968|0971|0972|0973|0974|0975|0976|0981|0982|0983|0984|0985|099)(-)[0-9]{7}$')
+
