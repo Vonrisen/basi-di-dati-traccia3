c164052e5a2970f97c1dfe4df4558e69066299df
Vonrisen <68206419+Vonrisen@users.noreply.github.com>
2021-02-26 19:40:30 +0100

    Update TablesAndConstraints.sql
    Aggiornamento constraint e creazione tabella
    
    Co-Authored-By: FabioCinicolo <75443422+FabioCinicolo@users.noreply.github.com>

--- Dump DB/TablesAndConstraints.sql
+++ Dump DB/TablesAndConstraints.sql
@@ -1,14 +1,15 @@
+-- Creazione sequenze
+
 CREATE SEQUENCE shop_sequence;
 CREATE SEQUENCE meal_sequence;
-CREATE SEQUENCE customerorder_sequence MAXVALUE 99999999999;
-CREATE SEQUENCE letter START WITH 65 MINVALUE 65 MAXVALUE 90;
+CREATE SEQUENCE customer_order_sequence;
 CREATE SEQUENCE customer_sequence;
 
 -- Creazione tabella "Customer"
 
 CREATE TABLE customer (
 
-        id CHAR(8) PRIMARY KEY DEFAULT to_char(nextval('customer_sequence'),'00000000FM'),
+    id CHAR(8) PRIMARY KEY DEFAULT to_char(nextval('customer_sequence'),'00000000FM'),
 	cf CHAR(16),
 	name VARCHAR(50) NOT NULL,
 	surname VARCHAR(50) NOT NULL,
@@ -16,7 +17,7 @@ CREATE TABLE customer (
 	birth_date DATE NOT NULL,
 	birth_place VARCHAR(64) NOT NULL,
 	gender CHAR(1) NOT NULL,
-	cellphone CHAR(10) NOT NULL UNIQUE,
+	cellphone CHAR(10) NOT NULL UNIQUE, -- Da vedere l'unique
 	email VARCHAR(320) NOT NULL UNIQUE,
 	password VARCHAR(32) NOT NULL
 	
@@ -29,11 +30,12 @@ CREATE TABLE customer (
 	id CHAR(3) PRIMARY KEY DEFAULT to_char(nextval('shop_sequence'),'000FM'),
 	name VARCHAR(50) NOT NULL,
 	address VARCHAR(255) NOT NULL UNIQUE,
-	working_hours CHAR(11) DEFAULT '00:00-24:00',
-	closing_days VARCHAR(62) DEFAULT '',
+	working_hours CHAR(11) NOT NULL DEFAULT '00:00-24:00',
+	closing_days VARCHAR(62),
 	password VARCHAR(32) NOT NULL,
 	email VARCHAR(320) NOT NULL UNIQUE,
-   	home_phone CHAR(11) NOT NULL UNIQUE
+   	home_phone VARCHAR(12) NOT NULL UNIQUE
+	
 );
 
 -- Creazione tabella "Rider"
@@ -47,15 +49,15 @@ CREATE TABLE rider (
 	birth_date DATE NOT NULL,
 	birth_place VARCHAR(64) NOT NULL,
 	gender CHAR(1) NOT NULL,
-	cellphone CHAR(10) NOT NULL UNIQUE,
+	cellphone CHAR(10) NOT NULL UNIQUE, -- Da vedere l'unique
 	
 	vehicle VARCHAR(18) NOT NULL, 
-	working_hours CHAR(11) NOT NULL, 
+	working_hours CHAR(11) NOT NULL DEFAULT '00:00-24:00', 
 	deliveries_number SMALLINT NOT NULL DEFAULT 0,
 	
 	shop_id CHAR(3),
-	FOREIGN KEY (shop_id) REFERENCES shop(id) ON DELETE CASCADE
-	--on delete set null
+	FOREIGN KEY (shop_id) REFERENCES shop(id) ON DELETE CASCADE -- Da rivedere i constraint ?on delete set null?
+
 );
 
 -- Creazione tabella "Meal"
@@ -74,21 +76,21 @@ CREATE TABLE meal (
 
 CREATE TABLE customerorder (
 
-	id CHAR(12) PRIMARY KEY DEFAULT to_char(nextval('customerorder_sequence'),'000000000000FM'),
+	id CHAR(12) PRIMARY KEY DEFAULT to_char(nextval('customer_order_sequence'),'000000000000FM'),
 	date TIMESTAMP NOT NULL DEFAULT current_timestamp,
 	delivery_time TIME,
 	address VARCHAR(255) NOT NULL,
 	status VARCHAR(20) NOT NULL DEFAULT 'In attesa',
-	payment VARCHAR(12) NOT NULL,
+	payment VARCHAR(32) NOT NULL,
 	note VARCHAR(255),
 	
 	rider_cf CHAR(16),
 	shop_id CHAR(3),
 	customer_id CHAR(8),
 	
-	FOREIGN KEY(rider_cf) REFERENCES Rider(cf),
-	FOREIGN KEY(shop_id) REFERENCES Shop(id),
-	FOREIGN KEY(customer_id) REFERENCES Customer(id)
+	FOREIGN KEY(rider_cf) REFERENCES Rider(cf), -- Da rivedere i constraint
+	FOREIGN KEY(shop_id) REFERENCES Shop(id), -- Da rivedere i constraint
+	FOREIGN KEY(customer_id) REFERENCES Customer(id) -- Da rivedere i constraint
 	
 );
 
@@ -109,8 +111,8 @@ CREATE TABLE ordercomposition (
 	quantity SMALLINT NOT NULL,
 
 	PRIMARY KEY(order_id, meal_id),
-	FOREIGN KEY (order_id) REFERENCES CustomerOrder(id) ON DELETE CASCADE,
-	FOREIGN KEY (meal_id) REFERENCES Meal(id)
+	FOREIGN KEY (order_id) REFERENCES CustomerOrder(id) ON DELETE CASCADE, -- Da rivedere i constraint
+	FOREIGN KEY (meal_id) REFERENCES Meal(id) -- Da rivedere i constraint
 	
 );
 
@@ -122,8 +124,8 @@ CREATE TABLE mealcomposition (
 	allergen_name VARCHAR(32), 
 
 	PRIMARY KEY(meal_id,allergen_name), 
-	FOREIGN KEY (meal_id) REFERENCES Meal(id) ON DELETE CASCADE,
-	FOREIGN KEY (allergen_name) REFERENCES Allergen(name) ON DELETE CASCADE
+	FOREIGN KEY (meal_id) REFERENCES Meal(id) ON DELETE CASCADE, -- Da rivedere i constraint
+	FOREIGN KEY (allergen_name) REFERENCES Allergen(name) ON DELETE CASCADE -- Da rivedere i constraint
 	
 );
 
@@ -134,8 +136,8 @@ CREATE TABLE Supply (
 	shop_id char(3),
 	meal_id char(4),
 	PRIMARY KEY (shop_id,meal_id),
-	FOREIGN KEY (shop_id) REFERENCES Shop(id) ON DELETE CASCADE,
-	FOREIGN KEY (meal_id) REFERENCES Meal(id) ON DELETE CASCADE
+	FOREIGN KEY (shop_id) REFERENCES Shop(id) ON DELETE CASCADE, -- Da rivedere i constraint
+	FOREIGN KEY (meal_id) REFERENCES Meal(id) ON DELETE CASCADE -- Da rivedere i constraint
 
 );
 
@@ -167,38 +169,38 @@ ADD CONSTRAINT shop_address_check CHECK (address~*'^(accesso|arco|belvedere|borg
 
 --Constraint email customer
 ALTER TABLE Customer
-ADD CONSTRAINT customer_email_check CHECK (email ~* '^[A-Za-z0-9._%-]+(@)[A-Za-z0-9.-]+[.][A-Za-z]+$'::text);
+ADD CONSTRAINT customer_email_check CHECK (email ~* '^[A-Za-z0-9._%-]+(@)[A-Za-z0-9.-]+[.][A-Za-z]+$'::text); -- OK
 
 ALTER TABLE Customer
-ADD CONSTRAINT customer_cf_check CHECK (cf ~* '^[a-zA-Z]{6}[0-9]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9]{2}([a-zA-Z]{1}[0-9]{3})[a-zA-Z]{1}$');
+ADD CONSTRAINT customer_cf_check CHECK (cf ~* '^[a-zA-Z]{6}[0-9]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9]{2}([a-zA-Z]{1}[0-9]{3})[a-zA-Z]{1}$'); -- OK
 
 --Constraint closing_days shop
 ALTER TABLE Shop 
-ADD CONSTRAINT shop_closing_days_check CHECK (closing_days ~* '^((lunedì|martedì|mercoledì|giovedì|venerdì|sabato|domenica)(, (?!$)|$))*$');
+ADD CONSTRAINT shop_closing_days_check CHECK (closing_days ~* '^((lunedì|lunedi|martedì|martedi|mercoledì|mercoledi|giovedì|giovedi|venerdì|venerdi|sabato|domenica)(, (?!$)|$))*$'); -- OK
 
 --Constraint working_hours shop
 ALTER TABLE Shop
-ADD CONSTRAINT shop_working_hours_check CHECK (working_hours ~* '^([0-2]{1}[0-9]{1})(:)([0-5]{1}[0-9]{1})(-)([0-2]{1}[0-9]{1})(:)([0-5]{1}[0-9]{1})$');
+ADD CONSTRAINT shop_working_hours_check CHECK (working_hours ~* '^([0-2]{1}[0-9]{1})(:)([0-5]{1}[0-9]{1})(-)([0-2]{1}[0-9]{1})(:)([0-5]{1}[0-9]{1})$'); -- OK
 
 --Constraint email shop
 ALTER TABLE Shop
-ADD CONSTRAINT shop_email_check CHECK (email ~* '^[A-Za-z0-9._%-]+(@foodoverflow.it)$');
+ADD CONSTRAINT shop_email_check CHECK (email ~* '^[A-Za-z0-9._%-]+(@foodoverflow.it)$'); -- OK
 
 --Constraint working_hours rider
 ALTER TABLE Rider 
-ADD CONSTRAINT rider_working_hours_check CHECK (working_hours ~* '^([0-2]{1}[0-9]{1})(:)([0-5]{1}[0-9]{1})(-)([0-2]{1}[0-9]{1})(:)([0-5]{1}[0-9]{1})$');
+ADD CONSTRAINT rider_working_hours_check CHECK (working_hours ~* '^([0-2]{1}[0-9]{1})(:)([0-5]{1}[0-9]{1})(-)([0-2]{1}[0-9]{1})(:)([0-5]{1}[0-9]{1})$'); -- OK
 
 --Constraint codice fiscale rider
 ALTER TABLE Rider
-ADD CONSTRAINT rider_cf_check CHECK (cf ~* '^[a-zA-Z]{6}[0-9]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9]{2}([a-zA-Z]{1}[0-9]{3})[a-zA-Z]{1}$');
+ADD CONSTRAINT rider_cf_check CHECK (cf ~* '^[a-zA-Z]{6}[0-9]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9]{2}([a-zA-Z]{1}[0-9]{3})[a-zA-Z]{1}$'); -- OK
 
 --Constraint cellphone customer
 ALTER TABLE Customer
-ADD CONSTRAINT customer_cellphone_check CHECK (cellphone ~ '^[0-9]{10}$');
+ADD CONSTRAINT customer_cellphone_check CHECK (cellphone ~ '^[0-9]{10}$'); -- OK
 
 --Constraint cellphone rider
 ALTER TABLE Rider
-ADD CONSTRAINT rider_cellphone_check CHECK (cellphone ~ '^[0-9]{10}$');
+ADD CONSTRAINT rider_cellphone_check CHECK (cellphone ~ '^[0-9]{10}$'); -- OK
 
 --Constraint home_phone
 ALTER TABLE Shop
@@ -207,5 +209,32 @@ ADD CONSTRAINT shop_home_phone_check CHECK (home_phone ~ '^(004191|010|011|0121|
 0439|0442|0444|0445|045|0461|0462|0463|0464|0465|0471|0472|0473|0474|0481|049|050|051|0521|0522|0523|0524|0525|0532|0533|0534|0535|0536|0541|0542|0543|0544|0545|0546|0547|0549|055 0564|0565|0566|0571|
 0572|0573|0574|0575|0577|0578|0583|0584|0585|0586|0587|0588|059 06|0623|070 071|0721|0722|0731|0732|0733|0734|0735|0736|0737|0742|0743|0744|0746|075|0761|0763|0765|0766|0771|0773|0774|0775|0776|0781|
 0782|0783|0784|0785|0789|079|080|081|0823|0824|0825|0827|0828|0831|0832|0833|0835|0836|085|0861|0862|0863|0864|0865|0871|0872|0873|0874|0875|0881|0882|0883|0884|0885|089|090|091|0921|0922|0923|
-0924|0925|0931|0932|0933|0934|0935|0941|0942|095|0961|0962|0963|0964|0965|0966|0967|0968|0971|0972|0973|0974|0975|0976|0981|0982|0983|0984|0985|099)(-)[0-9]{7}$');
+0924|0925|0931|0932|0933|0934|0935|0941|0942|095|0961|0962|0963|0964|0965|0966|0967|0968|0971|0972|0973|0974|0975|0976|0981|0982|0983|0984|0985|099)(-)[0-9]{7}$'); -- OK
+
+--Constraint deliveris_number rider
+ALTER TABLE Rider
+ADD CONSTRAINT rider_delieveries_number_check CHECK deliveries_number >= 0;
+
+--Constraint price meal
+ALTER TABLE Meal
+ADD CONSTRAINT meal_price_check CHECK price > 0;
+--Constraint quantity OrderComposition
+ALTER TABLE OrderComposition
+ADD CONSTRAINT order_composition_quantity_check CHECK quantity > 0;
+-- Constraint status CustomerOrder
+ALTER TABLE CustomerOrder
+ADD CONSTRAINT customer_order_status_check CHECK (status ~* '^(In attesa|In consegna|Consegnato|Errore)$');
+-- Constraint category Meal
+ALTER TABLE Meal
+ADD CONSTRAINT meal_category_check CHECK (category ~* '^(Primo piatto|Carne|Pesce|Pizza|Panino|Fritto|Dolce|Bevanda analcolica|Bevanda alcolica)$');
+-- Constraint name Allergen
+ALTER TABLE Allergen
+ADD CONSTRAINT allergen_name_check CHECK (name ~* '^(Cereali e derivati|Crostacei|Uova|Pesce|Arachidi|Soia|Latte e derivati|Frutta a guscio|Sedano|Senape|Sesamo|An. solforosa e solfiti|Lupini|Molluschi)$');
+-- Constraint gender Rider
+ALTER TABLE Rider
+ADD CONSTRAINT rider_gender_check CHECK (gender ~* '^(M|F)$');
+-- Constraint gender Customer
+ALTER TABLE Customer
+ADD CONSTRAINT customer_gender_check CHECK (gender ~* '^(M|F)$');
+
 
